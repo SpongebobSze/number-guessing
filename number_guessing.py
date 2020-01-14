40272d9e8fbae4001a6149e49d7991cfd3810895
Joey Sze <joeyshiqichen@outlook.com>
2020-01-14 21:31:29 +0800

    final version

--- number_guessing.py
+++ number_guessing.py
@@ -3,8 +3,14 @@
 # 猜对的话  印出“终于猜对了！”
 # 猜错的话  告诉他比答案大/小
 # 显示这是选手猜的第几次
+# 让玩家自己选择任意整数范围
+
 import random # 从图书馆里取书 即从package里面取module
-r = random.randint(1,100) # randint=random+int=随机整数
+start = input('请输入起始值: ')
+end = input('请输入终值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end) # randint=random+int=随机整数
 count = 0
 while True:
 	count += 1 # (count += 1) = (count = count + 1)
